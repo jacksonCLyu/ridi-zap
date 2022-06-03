f418ae64d8203f48d0dbba0cf3fbb392161618e8
young <yucl930904@163.com>
2022-06-04 03:06:57 +0800

    fix goroutine leak

--- zaplog/factory.go
+++ zaplog/factory.go
@@ -1,21 +1,44 @@
 package zaplog
 
 import (
-	"sync"
-
 	"github.com/jacksonCLyu/ridi-faces/pkg/logger"
+	"sync"
 )
 
-var logMemCache sync.Map
+var logMemCache = make(map[string]logger.Logger)
+var mu sync.RWMutex
 
 // GetLogger returns a logger with the given name.
 func GetLogger(name string) logger.Logger {
-	l, _ := logMemCache.LoadOrStore(name, ZapLogger(name))
-	return l.(logger.Logger)
+	if check(name) {
+		return logMemCache[name]
+	}
+	return store(name)
 }
 
 // GetLoggerWithOptions returns a logger with the given name and options.
 func GetLoggerWithOptions(name string, opts ...Option) logger.Logger {
-	l, _ := logMemCache.LoadOrStore(name, ZapLogger(name, opts...))
-	return l.(logger.Logger)
+	if check(name) {
+		return logMemCache[name]
+	}
+	return store(name, opts...)
+}
+
+func check(name string) bool {
+	mu.RLock()
+	defer mu.RUnlock()
+	_, ok := logMemCache[name]
+	return ok
+}
+
+func store(name string, opts ...Option) logger.Logger {
+	mu.Lock()
+	defer mu.Unlock()
+	l, ok := logMemCache[name]
+	if ok {
+		return l
+	}
+	l = ZapLogger(name, opts...)
+	logMemCache[name] = l
+	return l
 }
